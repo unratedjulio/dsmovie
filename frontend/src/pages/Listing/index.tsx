72ea705292f9b21975a41cca3b98aaeba3fc2b56
Julio Amorim <julio_cesar_antonio@hotmail.com>
2022-03-09 13:30:13 -0300

    Pagination layout

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,9 +1,12 @@
+import Pagination from "components/Pagination";
+
 function Listing() {
 
 
     return (
-        <h1>Página de listagem</h1>
+        <Pagination />
     );
+
 }
 
 export default Listing;
